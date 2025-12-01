5f44a052f23e8326a8ccdff2305e19bb2991599e
shreevardhini2812 <shreevardhini28122000@gmail.com>
2025-12-01 22:40:50 +0530

    url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://studentplannerapp.onrender.com/api",
 });
 
 // attach token automatically
